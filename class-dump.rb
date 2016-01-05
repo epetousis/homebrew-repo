832f61b3cdcae1effaaa6e9b03dc8baae0d75d56
Evan <evilGoldfish@users.noreply.github.com>
2016-01-05 16:12:58 +1100

    Update class-dump.rb

--- class-dump.rb
+++ class-dump.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class ClassDump < Formula
   homepage 'http://stevenygard.com/projects/class-dump/'
-  url 'https://github.com/nygard/class-dump/archive/3.5.tar.gz'
-  sha1 'c343bec63878161b02c956f49c9c1c8d989b4b5a'
+  url 'https://github.com/nygard/class-dump/archive/7acf1f00a03a219388a1d19d66edaa954c0be3a5.zip'
+  sha1 'f2aa0da5955c07876fac93ae6fbdb4cc11872292'
 
   def install
     xcodebuild "-project", "class-dump.xcodeproj", "-target", "all", "SYMROOT=build", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "-verbose", "install"
