8b923f07b2f5e2eafd8e19565ee134b71423a9b3
Evan <evilGoldfish@users.noreply.github.com>
2016-01-05 15:27:37 +1100

    Fixed incorrect SHA1 sum for class-dump, again

--- class-dump.rb
+++ class-dump.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ClassDump < Formula
   homepage 'http://stevenygard.com/projects/class-dump/'
   url 'https://github.com/nygard/class-dump/archive/3.5.tar.gz'
-  sha1 'e878d552ed175912ba4d8325718dda8dca680430'
+  sha1 'c343bec63878161b02c956f49c9c1c8d989b4b5a'
 
   def install
     xcodebuild "-project", "class-dump.xcodeproj", "-scheme", "all", "SYMROOT=build", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "-verbose", "install"
