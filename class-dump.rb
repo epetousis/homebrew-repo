48f358048a88d1ffae17d56a9586a14b7ef92e38
Evan <evilgoldfish@users.noreply.github.com>
2016-11-11 18:59:48 +1100

    Fixed out of date formula variable

--- class-dump.rb
+++ class-dump.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ClassDump < Formula
   homepage 'http://stevenygard.com/projects/class-dump/'
   url 'https://github.com/nygard/class-dump/archive/7acf1f00a03a219388a1d19d66edaa954c0be3a5.zip'
-  sha1 'f2aa0da5955c07876fac93ae6fbdb4cc11872292'
+  sha256 '039f91c526cb4f244d2eedf8f435c75fedb9cdd7a5111f2a60149ea3b8e9cbcb'
 
   def install
     xcodebuild "-project", "class-dump.xcodeproj", "-target", "all", "SYMROOT=build", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "-verbose", "install"
